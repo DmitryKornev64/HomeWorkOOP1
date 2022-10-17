3e35019b4b1177a64249daa2f256a28e4f9cb2b0
dmitry <dmitrykornev.64@gmail.com>
2022-10-17 21:45:26 +0300

    ДЗ ООП Задания 1-2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         // ДЗ 1-2
-        Human maxim = new Human("Максим", "Москва", 1999, "бренд-менеджером");
-        Human anya = new Human("Аня", "Саратов", 1983, "методистом образовательных программ");
-        Human katya = new Human("Катя", "Самара", 2003, "продакт-менеджером");
-        Human artem = new Human("Артем", "Ростов", 1959, "директором по развитию бизнеса");
+        Human maxim = new Human("Максим", "Минск", 1987, "бренд-менеджером");
+        Human anya = new Human("Аня", "Москва", 1993, "методистом образовательных программ");
+        Human katya = new Human("Катя", "Калининград", 1994, "продакт-менеджером");
+        Human artem = new Human("Артем", "Москва", 1995, "директором по развитию бизнеса");
         maxim.info();
         anya.info();
         katya.info();
