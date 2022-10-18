8ee608c6412b3b707fcad34aa419e0b633ba5778
dmitry <dmitrykornev.64@gmail.com>
2022-10-18 05:12:40 +0300

    ДЗ 3 Задание 1

--- src/Human.java
+++ src/Human.java
@@ -1,20 +1,44 @@
 public class Human {
     // ДЗ 1
-    String name;
+    String name ;
     String town;
+    int today = 2022;
     int yearOfBirth;
     String job; // ДЗ 2
 
+
     Human(String name, String town, int yearOfBirth, String job ) {
 
-        this.name = name;
-        this.town = town;
-        this.yearOfBirth = yearOfBirth;
-        this.job = job;
+        // Имя
+        if (name == null) {
+            this.name = "|Информация не указана|";
+        } else {
+            this.name = name;
+        }
+
+        // Город
+        if (town == null) {
+            this.town = "|Информация не указана|";
+        }else
+            this.town = town;
+
+        // Год рождения
+        if (yearOfBirth < 0) {
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = yearOfBirth;
+        }
+
+        // Должность
+        if (job == null) {
+            this.job = "|Информация не указана|";
+        } else {
+            this.job = job;
+        }
     }
 
     void info() {
-        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(ась) в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(ась) в " + (today - yearOfBirth) + " году. Я работаю на должности " + job + ". Будем знакомы!");
 
     }
 }
